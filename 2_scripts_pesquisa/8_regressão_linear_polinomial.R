5eac91f7196a652425ac6fe25965090ded59ec76
gessicaminski <gessicaminski@gmail.com>
2022-04-12 14:31:28 -0300

    Create 8_regressão_linear_polinomial.R

---
+++ 2_scripts_pesquisa/8_regressão_linear_polinomial.R
@@ -0,0 +1,5 @@
+### Correlacionar  o regime de chuva com a heterogeneidade das ZTEs ####
+
+#Parar testar se as heterogeneidades das ZTEs são correlacionadas a um regime de chuva mais variável (e.g., menor total acumulado anual e maiores variabilidades), utilizaram-se os modelos de regressão linear e polinomial. 
+#As variáveis dependentes testadas foram as métricas de heterogeneidade: IH, SD e IQR. Já as variáveis explicativas utilizadas foram MAP, MSI e CV. 
+
